60899587285620934e48006d245b70ce35d27ded
Nils Israel <info@nils-israel.net>
2009-03-25 19:58:01 +0100

    New macro: latex_inline (renders wiki page)
     * added css stylesheet, inline images now fit better in the text
     * added new macro latex_line(WikiPage), which renders a whole WikiPage

--- app/helpers/wiki_latex_helper.rb
+++ app/helpers/wiki_latex_helper.rb
@@ -2,16 +2,20 @@ require 'digest/sha2'
 require	'tempfile'
 
 module WikiLatexHelper
-  def render_image_tag(latex, classname)
-    render_to_string :template => 'wiki_latex/macro', :layout => false, :locals => {:classname=> classname, :name => latex.image_id, :source => latex.source}
+  def render_image_tag(latex)
+    render_to_string :template => 'wiki_latex/macro_inline', :layout => false, :locals => {:name => latex.image_id, :source => latex.source}
   end
 
+  def render_image_block(latex, wiki_name)
+    render_to_string :template => 'wiki_latex/macro_block', :layout => false, :locals => {:name => latex.image_id, :source => latex.source, :wiki_name => wiki_name}
+  end
 	class Macro
 		def initialize(view, source)
 		  @view = view
 		  @view.controller.extend(WikiLatexHelper)
 			@classname = source.index("<br />")!= nil ? "latex-block":"latex-inline" 
-			source = source.gsub("<br />","")
+			source.gsub!(/<br \/>/,"")
+			source.gsub!(/<\/?p>/,"")
 			name = Digest::SHA256.hexdigest(source)
 			if !WikiLatex.exists?(name)
 				@latex = WikiLatex.new(:source => source, :image_id => name)
@@ -22,7 +26,10 @@ module WikiLatexHelper
 		end
 
 		def render()
-		  @view.controller.render_image_tag(@latex, @classname)
+		  @view.controller.render_image_tag(@latex)
+		end
+		def render_block(wiki_name)
+		  @view.controller.render_image_block(@latex, wiki_name)
 		end
 	end
 end
--- init.rb
+++ init.rb
@@ -4,25 +4,40 @@ RAILS_DEFAULT_LOGGER.info 'Starting wiki_latex_plugin for Redmine'
 
 Redmine::Plugin.register :wiki_latex_plugin do
   name 'Latex Wiki-macro Plugin'
-  author 'soda'
+  author 'Nils Israel'
   description 'Render latex images'
-  version '0.0.2'
+  version '0.0.3'
 
 	Redmine::WikiFormatting::Macros.register do
 
 		desc <<'EOF'
 Latex Plugin
-{{latex(place latex code here)}}
+{{latex(place inline latex code here)}}
 
-{{latex(
-or place
-some code
-here
-)}}
+Don't use curly braces. '
 EOF
 		macro :latex do |wiki_content_obj, args|
-			m = WikiLatexHelper::Macro.new(self, args[0])
+			m = WikiLatexHelper::Macro.new(self, args.to_s)
 			m.render
 		end
-	end
+
+
+    # code borrowed from wiki template macro
+    desc <<'EOF'
+Include wiki page rendered with latex.
+{{latex_include(WikiName)}}
+EOF
+    macro :latex_include do |obj, args|
+      page = Wiki.find_page(args.to_s, :project => @project)
+      raise 'Page not found' if page.nil? || !User.current.allowed_to?(:view_wiki_pages, page.wiki.project)
+
+      @included_wiki_pages ||= []
+      raise 'Circular inclusion detected' if @included_wiki_pages.include?(page.title)
+      @included_wiki_pages << page.title
+      m = WikiLatexHelper::Macro.new(self, page.content.text)
+      @included_wiki_pages.pop
+      m.render_block(args.to_s)
+    end
+  end
+
 end
